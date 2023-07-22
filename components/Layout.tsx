18f0f5da2b345c24db7ec8284f5815f6988d5f77
mikelisrael <israelipinkz@gmail.com>
2023-07-22 04:32:52 +0100

    feat: build layout

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+
+      {children}
+    </>
+  );
+};
+
+export default Layout;

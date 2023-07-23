4adc9d494d64eb7dfd7fc52b5075417a0cecd334
mikelisrael <israelipinkz@gmail.com>
2023-07-23 16:23:44 +0100

    update: include contact on layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Layout from "@/components/Layout";
 import React from "react";
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <Layout>
       <Header />
+      <Contact />
     </Layout>
   );
 };

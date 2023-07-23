7a6a24e9257bbcde3910d414b0de51b6f9258c60
mikelisrael <israelipinkz@gmail.com>
2023-07-23 16:23:19 +0100

    feat: build navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import cn from "classnames";
-
-const activeLink =
-  "after:content-[''] after:bg-yellowPrimary after:h-2 after:w-2 after:left-1/2 after:-translate-x-1/2 after:-bottom-4 after:absolute after:rounded-full";
-
-const inactiveLink = "text-gray hover:text-textWhite";
 
 const Navbar = () => {
   // State to track the active link
   const [activeTab, setActiveTab] = useState<"services" | "projects" | "blog">(
     "services"
   );
+  const indicatorRef = useRef<HTMLLIElement>(null);
 
   const returnClassName = (tab: "services" | "projects" | "blog") => {
     return cn("relative cursor-pointer", {
-      [activeLink]: activeTab === tab,
-      [inactiveLink]: activeTab !== tab,
+      ["text-textWhite"]: activeTab === tab,
+      ["text-gray hover:text-textWhite"]: activeTab !== tab,
     });
   };
 
@@ -23,29 +19,63 @@ const Navbar = () => {
     setActiveTab(tab);
   };
 
+  useEffect(() => {
+    // Move the indicator to the center of the active link when the activeTab changes
+    if (indicatorRef.current) {
+      const activeTabElement =
+        indicatorRef.current.parentElement!.querySelector(
+          `[data-tab="${activeTab}"]`
+        );
+      if (activeTabElement) {
+        const parentRect =
+          indicatorRef.current.parentElement!.getBoundingClientRect();
+        const activeTabRect = activeTabElement.getBoundingClientRect();
+        const offsetX =
+          activeTabRect.left - parentRect.left + activeTabRect.width / 2;
+        indicatorRef.current.style.transform = `translateX(${offsetX}px)`;
+      }
+    }
+  }, [activeTab]);
+
   return (
-    <nav className="universal_x py-10 flex justify-between">
+    <nav className="universal_x py-5 my-5 flex justify-between sticky top-0 z-30">
+      {/* add class above for blur: backdrop-blur-md  */}
       <div className="w-16 h-16 bg-yellowPrimary" />
 
-      <ul className="flex text-3xl items-center gap-8">
+      <ul className="flex text-xl md:text-3xl items-center gap-5 md:gap-8 relative">
         <li
+          role="link"
+          tabIndex={activeTab === "services" ? 0 : -1}
+          data-tab="services"
           className={returnClassName("services")}
           onClick={() => handleClickedTab("services")}
         >
           Services
         </li>
         <li
+          role="link"
+          tabIndex={activeTab === "projects" ? 0 : -1}
+          data-tab="projects"
           className={returnClassName("projects")}
           onClick={() => handleClickedTab("projects")}
         >
           Projects
         </li>
         <li
+          role="link"
+          tabIndex={activeTab === "blog" ? 0 : -1}
+          data-tab="blog"
           className={returnClassName("blog")}
           onClick={() => handleClickedTab("blog")}
         >
           Blog
         </li>
+
+        {/* indicator */}
+        <li
+          ref={indicatorRef}
+          className="absolute bottom-0 md:-bottom-2 transition-all delay-200 indicator bg-yellowPrimary h-2 w-2 rounded-full"
+        ></li>
       </ul>
     </nav>
   );

d85578dd7b129b9f864ab63d12115c5e00fc1ab8
mikelisrael <israelipinkz@gmail.com>
2023-07-23 20:34:15 +0100

    test: use blurhash

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,45 +1,17 @@
 import React from "react";
+import LazyLoader from "./LazyLoader";
 
 const Contact = () => {
   return (
     <div className="bg-bgSecondary py-24">
       <div className="universal_x">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio numquam
-        laboriosam explicabo illum ullam pariatur, nihil sunt, cum sapiente
-        dolores minus recusandae ratione quae cumque accusantium quo itaque eos
-        minima. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quibusdam voluptate libero eum ab sunt pariatur dolores ullam facilis
-        vel repellat quas nemo amet fugit enim voluptatum qui labore, ipsa
-        placeat, molestiae delectus reprehenderit minus nihil neque quam? Et sed
-        expedita libero pariatur dolorem mollitia, natus dolore accusamus
-        veritatis error distinctio eos, modi aperiam, vel adipisci tempora autem
-        alias excepturi similique provident non ipsa quo. Ipsam iusto, esse
-        illum odio ea mollitia aut accusantium harum! Laborum obcaecati
-        voluptates tempora omnis exercitationem consequatur magni excepturi
-        soluta magnam voluptas nihil, deserunt repellat debitis mollitia minima!
-        Similique perferendis repudiandae animi, voluptatibus quaerat esse
-        libero ipsa non sint reiciendis necessitatibus aut ipsum quod ad nostrum
-        corporis velit sed natus molestiae? Consequuntur excepturi molestias
-        quia quaerat labore cumque ullam et modi dolores perferendis, eaque
-        ducimus minus vitae corrupti doloribus dicta amet repudiandae
-        repellendus explicabo saepe in dolor, nam fuga voluptatibus? Itaque
-        magni eos sequi, tempora optio in minima laborum nostrum ad dolores nemo
-        dolorum unde voluptatum quia omnis rem, cum consequatur animi distinctio
-        sit, cupiditate fuga nisi! Tempore nesciunt, sit molestiae ea, sed
-        dolore facere beatae repudiandae aperiam voluptas a? Quos laborum
-        cupiditate atque praesentium dicta, sunt earum dignissimos perspiciatis
-        at, similique tempore voluptas aut rerum deleniti adipisci vel?
-        Perferendis ab aliquam aspernatur rerum, quia consequatur corrupti
-        expedita quisquam dolor beatae, vel iste, amet tenetur quibusdam illum
-        eaque quas dolores suscipit? Amet autem enim alias animi assumenda, quos
-        dolorum omnis! Corrupti officia perferendis eos. Voluptatum sequi a
-        sunt! Repellendus explicabo totam placeat ipsam. Beatae exercitationem
-        maiores quia sequi eaque provident, natus itaque adipisci fugit aut
-        minima consequatur modi mollitia quibusdam repudiandae ipsum magni
-        commodi nemo facere esse placeat non repellendus! Consectetur odit
-        accusantium, facilis amet natus explicabo enim, rerum, ipsam perferendis
-        id ullam quo delectus? Minus, blanditiis quisquam tenetur iure delectus
-        maiores incidunt nihil quis rem.
+        <div className="h-[30rem]">
+          <LazyLoader
+            hash="LXI{~GoMoMxt~VNdoft7o#oet7oy"
+            src="/img/jj.jpg"
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );

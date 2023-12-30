d3a26af03f0536838b609c151e051256ec13084d
mikelisrael <israelipinkz@gmail.com>
2023-12-30 15:21:10 +0100

    Add image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["www.trulyao.dev"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

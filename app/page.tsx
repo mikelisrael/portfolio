47ade1c2bbfbcc082c308213a18a4eef059628ac
mikelisrael <israelipinkz@gmail.com>
2024-04-22 19:55:17 +0100

    Add sanityFetch for on-demand fetching of page info

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/components/home/contact";
 import Hero from "@/components/home/hero";
 import Projects from "@/components/home/projects";
 import { client } from "@/sanity/lib/client";
+import { sanityFetch } from "@/sanity/lib/onDemandFetch";
 import { IPageInfo } from "@/types";
 import { groq } from "next-sanity";
 
@@ -12,7 +13,10 @@ const query = groq`*[_type == "pageInfo"][0]{
 }`;
 
 const Home = async () => {
-  const pageInfo: IPageInfo = await client.fetch(query);
+  const pageInfo: IPageInfo = await sanityFetch({
+    query,
+    tags: ["pageInfo"], // will revalidate for page info changes
+  });
 
   return (
     <main className="pt-0 md:pt-10">

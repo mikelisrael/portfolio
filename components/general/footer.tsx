6f2a7904037afcae8fbf2b80f3d0c8f0f0ef4bd0
mikelisrael <israelipinkz@gmail.com>
2024-04-22 17:07:25 +0100

    Refactor footer component to fetch socials from Sanity CMS

--- components/general/footer.tsx
+++ components/general/footer.tsx
@@ -1,39 +1,17 @@
 "use client";
 
-import { FaRegCopyright } from "react-icons/fa6";
-import { useGlobalContext } from "../providers/context";
-import { socials } from "./socials";
+import { client } from "@/sanity/lib/client";
+import { ISocial } from "@/types";
+import { groq } from "next-sanity";
+import FooterContent from "./footerContent";
 
-const Footer = () => {
-  const { isFooterAndNavHidden } = useGlobalContext();
-
-  if (isFooterAndNavHidden) return null;
+const Footer = async () => {
+  const query = groq`*[_type == "social"]`;
+  const socials: ISocial[] = await client.fetch(query);
 
   return (
     <footer>
-      <div className="universal_x">
-        <div className="text-gray-400 flex flex-wrap-reverse items-center justify-between gap-x-2 gap-y-5 py-5 md:py-10">
-          <div className="flex gap-2 text-xs md:text-sm">
-            <span className="flex items-center gap-1">
-              <FaRegCopyright className="text-xs" /> 2023
-            </span>
-            <span>Michael Israel.</span>
-          </div>
-
-          {/* icons */}
-          <div className="flex items-center gap-3 text-lg">
-            {socials.map((social, idx) => (
-              <a
-                key={idx}
-                className="transition-colors duration-200 hover:text-primary"
-                href={social.link}
-              >
-                {social.Icon}
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
+      <FooterContent socials={socials} />
     </footer>
   );
 };

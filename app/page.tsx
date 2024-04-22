42077c780a4aaa36c1b5929e4e17ec1faa3d3ee5
mikelisrael <israelipinkz@gmail.com>
2024-04-22 17:04:26 +0100

    Refactor Home component to fetch pageInfo from Sanity

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import BlogSection from "@/components/home/blog-section";
 import Contact from "@/components/home/contact";
 import Hero from "@/components/home/hero";
 import Projects from "@/components/home/projects";
-import React from "react";
+import { client } from "@/sanity/lib/client";
+import { IPageInfo } from "@/types";
+import { groq } from "next-sanity";
 
-const Home = () => {
+const query = groq`*[_type == "pageInfo"][0]{
+  ...,
+  socials[]->
+}`;
+
+const Home = async () => {
+  const pageInfo: IPageInfo = await client.fetch(query);
+
   return (
     <main className="pt-0 md:pt-10">
-      <Hero />
-      <Contact />
+      <Hero {...pageInfo} />
+      <Contact {...pageInfo} />
       <Projects />
       <BlogSection />
     </main>

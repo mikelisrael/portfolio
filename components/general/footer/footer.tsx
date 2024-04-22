b1962d5620781c9da4698b068fcbf6033f893682
mikelisrael <israelipinkz@gmail.com>
2024-04-22 17:27:56 +0100

    Add footer component and footer content component

---
+++ components/general/footer/footer.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { client } from "@/sanity/lib/client";
+import { ISocial } from "@/types";
+import { groq } from "next-sanity";
+import FooterContent from "./footerContent";
+
+const Footer = async () => {
+  const query = groq`*[_type == "social"]`;
+  const socials: ISocial[] = await client.fetch(query);
+
+  return (
+    <footer>
+      <FooterContent socials={socials} />
+    </footer>
+  );
+};
+
+export default Footer;

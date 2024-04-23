91448c5612f470b17564ff4b5d5b34218dc7e81f
mikelisrael <israelipinkz@gmail.com>
2024-04-23 19:47:08 +0100

    Add tool schema type to the Sanity schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -7,7 +7,17 @@ import author from "./schemaTypes/author";
 import project from "./schemaTypes/project";
 import pageInfo from "./schemaTypes/pageInfo";
 import social from "./schemaTypes/social";
+import tool from "./schemaTypes/tool";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [pageInfo, social, post, author, category, blockContent, project],
+  types: [
+    pageInfo,
+    social,
+    tool,
+    post,
+    author,
+    category,
+    blockContent,
+    project,
+  ],
 };

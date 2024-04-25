4ca6eefc64dbb2c9e4d22ebae3b7ee1126f24b98
mikelisrael <israelipinkz@gmail.com>
2024-04-25 23:55:42 +0100

    Refactor utils.ts: Add emailJSConfig and trimData functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const emailJSConfig = {
+  serviceID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+  templateID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  publicKey: process.env.NEXT_PUBLIC_EMAILJS_KEY,
+};
+
+export const trimData = (data: any) => {
+  const trimmedData = Object.keys(data).reduce((acc, key) => {
+    return { ...acc, [key]: data[key].trim() };
+  }, {});
+
+  return trimmedData;
+};

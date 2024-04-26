7154dacf852da0c7aed8470fde547869ae6dcbbe
mikelisrael <israelipinkz@gmail.com>
2024-04-26 01:40:34 +0100

    Refactor footer component to include dynamic name and current year

--- components/general/footer/footer-client.tsx
+++ components/general/footer/footer-client.tsx
@@ -7,10 +7,15 @@ import Socials from "../socials";
 
 type Props = {
   socials: ISocial[];
+  name: string;
 };
 
-function FooterContent({ socials }: Props) {
+function FooterContent({ socials, name }: Props) {
   const { isFooterAndNavHidden } = useGlobalContext();
+
+  function currentYear() {
+    return new Date().getFullYear();
+  }
 
   if (isFooterAndNavHidden) return null;
 
@@ -19,9 +24,9 @@ function FooterContent({ socials }: Props) {
       <div className="text-gray-400 flex flex-wrap-reverse items-center justify-between gap-x-2 gap-y-5 py-5 md:py-10">
         <div className="flex gap-2 text-xs md:text-sm">
           <span className="flex items-center gap-1">
-            <FaRegCopyright className="text-xs" /> 2024
+            <FaRegCopyright className="text-xs" /> {currentYear()}
           </span>
-          <span>Michael Israel.</span>
+          <span>{name}.</span>
         </div>
 
         {/* icons */}

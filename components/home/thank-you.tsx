0cf74e1c4731fc61100a51dc2158649848f6bdc6
mikelisrael <israelipinkz@gmail.com>
2024-04-26 02:29:32 +0100

    Add logo image to ThankYou component

--- components/home/thank-you.tsx
+++ components/home/thank-you.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { AnimatedUpComponent } from "../general/animated-components";
+import Image from "next/image";
 
 const ThankYou = () => {
   return (
-    <div className="py-20">
+    <div className="flex flex-col items-center justify-center gap-7 py-20 md:gap-10">
+      <AnimatedUpComponent className="relative size-14 md:size-20">
+        <Image src="/logo.png" alt="logo" fill />
+      </AnimatedUpComponent>
       <AnimatedUpComponent>
-        <p className="text-center">
+        <p>
           <span className="font-black">Thanks for scrolling. </span>
           That&rsquo;s all folks!
         </p>

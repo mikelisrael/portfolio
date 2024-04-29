47aca361e4990e45ab99ef130314774bab0edb0d
mikelisrael <israelipinkz@gmail.com>
2024-04-29 19:30:04 +0100

    Refactor blog header component to use utility function and dynamic post count

--- components/blog/header.tsx
+++ components/blog/header.tsx
@@ -2,24 +2,25 @@
 
 import React from "react";
 import { useGlobalContext } from "../providers/context";
-import moment from "moment";
+import { getDate } from "@/lib/utils";
+import { IPost } from "@/types";
 
-const Header = () => {
+const Header = ({ posts }: { posts: IPost[] }) => {
   const { headerRef } = useGlobalContext();
 
   return (
-    <header ref={headerRef} className="relative pt-10 md:pt-0">
+    <header ref={headerRef} className="relative">
       <h2
         aria-label="blog header"
-        className="mb-1 mt-3 w-full text-3xl font-semibold duration-500 animate-in fade-in slide-in-from-right-48 sm:text-5xl"
+        className="mb-1 w-full text-3xl font-semibold duration-500 animate-in fade-in slide-in-from-right-48 sm:text-5xl"
       >
         Stories
       </h2>
 
       <p className="space-x-2 text-foreground-secondary duration-700 animate-in fade-in slide-in-from-right-48 ">
-        <span>{moment().format("Do MMMM, YYYY")}</span>
-        <span className="text-lg">•</span>
-        <span>3 Stories</span>
+        <span>{getDate()}</span>
+        <span className="text-lg text-primary">•</span>
+        <span>{posts.length} Stories</span>
       </p>
     </header>
   );

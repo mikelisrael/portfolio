3447b18a66414a322aa98bf17da61b8659393eba
mikelisrael <israelipinkz@gmail.com>
2024-04-30 22:59:06 +0100

    Add metadata for projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,12 @@
 import Header from "@/components/projects/header";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Projects • Michael Israel",
+  description:
+    "I am a software engineer who loves to build and create. Here are some of the projects I have worked on over the years.",
+};
 
 const Projects = () => {
   return (

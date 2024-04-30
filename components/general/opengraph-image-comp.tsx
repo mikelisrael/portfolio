bbe4a36bf1593ae6ffd6c720d3edb47ef38f00c3
mikelisrael <israelipinkz@gmail.com>
2024-05-01 00:49:57 +0100

    Refactor OpenGraphImageComponent styling

--- components/general/opengraph-image-comp.tsx
+++ components/general/opengraph-image-comp.tsx
@@ -4,14 +4,48 @@ type Props = { title: string };
 
 function OpenGraphImageComponent({ title }: Props) {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-3 bg-background-secondary p-10">
-      <div className="relative size-20">
-        <Image src="/logo.png" alt="logo" fill />
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        fontSize: 128,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "0.75rem",
+        backgroundColor: "#333647",
+        padding: "2.5rem",
+        color: "#eaeaeb",
+      }}
+    >
+      <div
+        className="relative size-20"
+        style={{
+          width: "80px",
+          height: "80px",
+          backgroundImage: `url(https://mikelisrael.vercel.app/logo.png)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {/* <Image src="/logo.png" alt="logo" fill /> */}
       </div>
 
-      <h2 className="mt-6 line-clamp-2 text-3xl font-bold">{title}</h2>
+      <h2
+        // className="mt-6 line-clamp-2 text-3xl font-bold"
+        style={{
+          fontSize: "30px",
+          fontWeight: 700,
+          WebkitLineClamp: 2,
+          overflow: "hidden",
+        }}
+      >
+        {title}
+      </h2>
 
-      <small className="text-sm">Michael Israel</small>
+      <small style={{ fontSize: "14px" }}>Michael Israel</small>
     </div>
   );
 }

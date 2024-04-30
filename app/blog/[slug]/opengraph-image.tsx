c89bd15ef04aafc8df9588ee435534952d9a01b0
mikelisrael <israelipinkz@gmail.com>
2024-05-01 00:07:46 +0100

    Add opengraph-image component for blog posts

---
+++ app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,47 @@
+import OpenGraphImageComponent from "@/components/general/opengraph-image-comp";
+import { client } from "@/sanity/lib/client";
+import { IPost } from "@/types";
+import { groq } from "next-sanity";
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const alt = "Blog post image";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const titleQuery = groq`*[_type == "post" && slug.current == $slug][0]{title}`;
+  const res: IPost = await client.fetch(titleQuery, { slug });
+
+  // Font
+  //   const interSemiBold = fetch(
+  //     new URL("./Inter-SemiBold.ttf", import.meta.url),
+  //   ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    <OpenGraphImageComponent title={res.title} />,
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      //   fonts: [
+      //     {
+      //       name: "Inter",
+      //       data: await interSemiBold,
+      //       style: "normal",
+      //       weight: 400,
+      //     },
+      //   ],
+    },
+  );
+}

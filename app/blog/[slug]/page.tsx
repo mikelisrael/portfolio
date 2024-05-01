cc316a7594920876271bf5b3b0a45eccdaa8258b
Michael Israel <57787893+mikelisrael@users.noreply.github.com>
2024-05-01 15:06:48 +0100

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -42,15 +42,16 @@ export async function generateMetadata(
     title: res.title,
     description,
     openGraph: {
-      images: [
+      //images: [
       //  currentImage
-        {
-         type: "image/png",
-         width: 1200,
-         height: 630,
-         url: `/opengraph/${slug}`
-        }
-        , ...previousImages],
+       // {
+      //  type: "image/png",
+      //  width: 1200,
+      //  height: 630,
+      //  url: `/opengraph/${slug}`
+     //  }
+      //  , 
+    //...previousImages],
       description,
     },
   };

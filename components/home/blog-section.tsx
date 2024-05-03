0a65acb63e7be59de5484ab797e5727d945db5e2
mikelisrael <israelipinkz@gmail.com>
2024-05-03 20:12:28 +0100

    Refactor blog section component and add dynamic data

--- components/home/blog-section.tsx
+++ components/home/blog-section.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 import {
@@ -6,27 +7,20 @@ import {
   AnimatedUpComponent,
 } from "../general/animated-components";
 import { useGlobalContext } from "../providers/context";
+import moment from "moment";
 
-const BlogSection = () => {
+interface Props {
+  posts: {
+    title: string;
+    publishedAt: string;
+    slug: string;
+  }[];
+}
+
+const returnDate = (date: string) => moment(date).format("MMM DD");
+
+const BlogSection = ({ posts }: Props) => {
   const { blogRef } = useGlobalContext();
-
-  const blogs = [
-    {
-      title: "Let this be a lesson to you",
-      date: "Feb 26",
-      link: "https://www.trulyao.dev",
-    },
-    {
-      title: "How do you use time tracking to improve your productivity?",
-      date: "Mar 15",
-      link: "https://www.trulyao.dev/second-blog",
-    },
-    {
-      title: "Third blog title",
-      date: "Mar 30",
-      link: "https://www.trulyao.dev/third-blog",
-    },
-  ];
 
   return (
     <section
@@ -34,7 +28,7 @@ const BlogSection = () => {
       ref={blogRef}
       className="scroll-m-20 bg-background-secondary"
     >
-      <div className="universal_x grid gap-x-10 gap-y-10 py-24 md:grid-cols-[350px,1fr] md:py-32 lg:gap-x-28">
+      <div className="universal_x grid gap-x-10 gap-y-10 py-24 md:grid-cols-[300px,1fr] md:py-32 lg:grid-cols-[350px,1fr] lg:gap-x-28">
         <div>
           <AnimatedUpComponent>
             <h6 className="text-xs tracking-[0.2em] text-foreground-secondary md:text-sm">
@@ -53,30 +47,32 @@ const BlogSection = () => {
         </div>
 
         <div className="overflow-x-hidden">
-          {blogs.map((blog, idx) => (
-            <AnimatedLeftComponent
-              delay={0.2}
-              key={idx}
-              className="border-b border-gray last-of-type:border-0"
-            >
-              <Link
-                href="/"
-                className="group grid grid-cols-[auto,1fr,auto] items-center gap-5 py-5 md:gap-5 lg:gap-10 lg:py-10 xl:gap-20"
+          {posts.length > 0 &&
+            posts.map((blog, idx) => (
+              <AnimatedLeftComponent
+                delay={0.2}
+                key={idx}
+                className="border-b border-gray last-of-type:border-0"
               >
-                <span className="space-x-0.5 text-xs text-foreground-secondary md:space-x-2 md:text-sm lg:space-x-1">
-                  <span>{blog.date}</span>
-                  <span>•</span>
-                  <span>Blog</span>
-                </span>
+                <Link
+                  href={`/blog/${blog.slug}`}
+                  // TODO: check the grid 110 col in xs -> sm
+                  className="group grid grid-cols-[105px,1fr,auto]  items-center gap-5 py-5 md:grid-cols-[110px,1fr,auto] md:gap-5 lg:gap-10 lg:py-10 xl:gap-20"
+                >
+                  <span className="space-x-0.5 text-xs text-foreground-secondary md:space-x-2 md:text-sm lg:space-x-1">
+                    <span>{returnDate(blog.publishedAt)}</span>
+                    <span>•</span>
+                    <span>Blog</span>
+                  </span>
 
-                <h4 className="text-sm font-bold sm:text-base md:text-xl">
-                  {blog.title}
-                </h4>
+                  <h4 className="text-sm font-bold sm:text-base md:text-xl">
+                    {blog.title}
+                  </h4>
 
-                <FaArrowRight className="inline-block -translate-x-3 text-primary duration-200 group-hover:translate-x-0" />
-              </Link>
-            </AnimatedLeftComponent>
-          ))}
+                  <FaArrowRight className="inline-block -translate-x-3 text-primary duration-200 group-hover:translate-x-0" />
+                </Link>
+              </AnimatedLeftComponent>
+            ))}
         </div>
       </div>
     </section>

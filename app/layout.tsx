40b503be8116ca60a1e60e3804a56f3efb8b8b6e
mikelisrael <israelipinkz@gmail.com>
2024-05-11 10:59:57 +0100

    Remove unnecessary div tag in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,6 @@ export default function RootLayout({
             {children}
             <Footer />
           </div>
-
           <Toaster richColors position="top-center" />
         </Providers>
       </body>

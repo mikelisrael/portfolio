ee659b6fdd0f3a4d21e2bdb3b02609f2ec0fbf4e
mikelisrael <israelipinkz@gmail.com>
2024-05-12 17:27:42 +0100

    Refactor projects header component

--- components/projects/header.tsx
+++ components/projects/header.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useGlobalContext } from "../providers/context";
 
-const Header = () => {
+const Header = ({ intro }: { intro: string }) => {
   const { headerRef } = useGlobalContext();
 
   return (
@@ -11,7 +11,7 @@ const Header = () => {
       ref={headerRef}
       className="universal_x relative flex items-center justify-center pb-10 pt-10 md:pt-0"
     >
-      <div className="max-w-2xl space-y-5 text-center">
+      <div className="space-y-5 text-center">
         <h2
           aria-label="projects header"
           className="mb-1 mt-3 w-full text-3xl font-semibold duration-500 animate-in fade-in slide-in-from-bottom-48 sm:text-5xl"
@@ -19,10 +19,7 @@ const Header = () => {
           Projects
         </h2>
         <p className="text-foreground-secondary duration-700 animate-in fade-in slide-in-from-bottom-48">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A provident
-          voluptates cum earum dolorem placeat est quo harum iusto, accusamus
-          quasi aspernatur quae autem! At officia accusamus debitis
-          necessitatibus facere.
+          {intro}
         </p>
       </div>
     </header>

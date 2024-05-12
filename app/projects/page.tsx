89fde4f34c8ac47b893115d0a57c8d2c6c33ad10
mikelisrael <israelipinkz@gmail.com>
2024-05-12 17:27:15 +0100

    Add projects list and fetch projects data from Sanity

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,9 @@
 import Header from "@/components/projects/header";
+import ProjectsList from "@/components/projects/projects-list";
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { ProjectsData } from "@/types";
 import { Metadata } from "next";
-import React from "react";
+import { groq } from "next-sanity";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -8,10 +11,21 @@ export const metadata: Metadata = {
     "I am a software engineer who loves to build and create. Here are some of the projects I have worked on over the years.",
 };
 
-const Projects = () => {
+const query = groq`{
+  "projectsIntro": *[_type == "pageInfo"][0].projectsIntro,
+  "projects": *[_type == "project"]
+}`;
+
+const Projects = async () => {
+  const projectsInfo: ProjectsData = await sanityFetch({
+    query: query,
+    tags: ["pageInfo", "project"],
+  });
+
   return (
     <main className="pb-32 pt-10">
-      <Header />
+      <Header intro={projectsInfo.projectsIntro} />
+      <ProjectsList projects={projectsInfo.projects} />
     </main>
   );
 };

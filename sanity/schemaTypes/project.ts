7c6d94e930524288f24c3392c31d03d697603c93
mikelisrael <israelipinkz@gmail.com>
2024-05-13 13:15:21 +0100

    Add slug field to project schema

--- sanity/schemaTypes/project.ts
+++ sanity/schemaTypes/project.ts
@@ -18,6 +18,16 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: "description",
       title: "Description",
       type: "text",

c9263a09d2526ac26354d012828b0bf33844e066
mikelisrael <israelipinkz@gmail.com>
2024-12-21 11:07:12 +0100

    chore: export blog components from one place

---
+++ components/blog/index.ts
@@ -0,0 +1,7 @@
+import BlogList from "./blog-list";
+import GoBackButton from "./go-back-button";
+import ImportPreviewBlog from "./import-preview-Blog";
+import SingleBlogPost from "./single-blog-post";
+
+
+export { BlogList, GoBackButton, ImportPreviewBlog, SingleBlogPost };
